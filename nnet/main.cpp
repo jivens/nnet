707ff723c7979067301181b64edb9bda7d045bc2
John Ivens <jivens@johns-macbook-pro.local>
2016-05-17 16:20:07 -0700

    Initial Commit

---
+++ nnet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nnet
+//
+//  Created by John Ivens on 5/17/16.
+//  Copyright © 2016 GalacticCore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
